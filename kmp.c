9d5a2607ecc302a8ec202072fe3a6e316e6e8df0
huangningcompsci <haoqu595@gmail.com>
2026-05-18 22:07:27 +0800

    kmp算法改进，改用nextval,实现了失配时的更优选择

--- kmp.c
+++ kmp.c
@@ -1,6 +1,6 @@
 #include <string.h>
 #include <stdio.h>
-
+/*
 int Initnext(int *next,char *p)
 {
     int k=0;
@@ -15,14 +15,38 @@ int Initnext(int *next,char *p)
                 k++;
                 next[i]=k;
             }
-            else{k=next[k];}        /*为了找一个更小的相等前后缀。因为本身就有大相等前后缀，
-    }                               由于相等：前后缀是相等的，故找前缀的next值来快速判断是否有更小相等前后缀*/
+            else{k=next[k];}        为了找一个更小的相等前后缀。因为本身就有大相等前后缀，
+    }                               由于相等：前后缀是相等的，故找前缀的next值来快速判断是否有更小相等前后缀
+    return 1;
+}
+*/
+
+int Initnextval(int *nextval,char *p)
+{
+    int k=0;
+    int i=1;
+    
+    nextval[1]=0;
+    while(i<strlen(p))
+    {
+        if(p[i]==p[k]||k==0)
+            {
+                i++;
+                k++;
+                if(p[i]!=p[k])          //若相等，说明失配时这个子串里面准备比较的也必定不匹配，相当于(a1=a2=a && a1!=b1 )=>a2!=b1
+                    nextval[i]=k;
+                else
+                    nextval[i]=nextval[k];  //等于nextval[k]的原因：相当于先做一步k=nextval[k]，换了一个更小的相等前后缀
+            }
+            else{k=nextval[k];}        /*为了找一个更小的相等前后缀。因为本身就有大相等前后缀，
+                            由于相等：前后缀是相等的，故找前缀的next值来快速判断是否有更小相等前后缀*/
+    }                               
     return 1;
 }
 
 int kmp(char *big,char *sma)
-{int next[100];
-Initnext(next,sma);
+{int nextval[100];
+Initnextval(nextval,sma);
 int i=0,j=0;
 while(i<strlen(big)&&j<strlen(sma))
     {
@@ -33,7 +57,7 @@ while(i<strlen(big)&&j<strlen(sma))
             }
         else
             {
-                j=next[j];
+                j=nextval[j];
             }    
     }
 
