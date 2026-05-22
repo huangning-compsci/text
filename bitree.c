686da26cd954df7e9156802f424269ef690cc40b
huangningcompsci <haoqu595@gmail.com>
2026-05-22 19:43:13 +0800

    线索树的线索化（建立在已有一个串联起来的二叉树的基础上）,遍历

--- bitree.c
+++ bitree.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct BitNode
-{
-    int data;
-    struct BitNode *Lchild,*Rchild;
-}BitNode,*BiTree;
+#define OK 1;
 
 void InitBiTree(BiTree *T)
 {char in;
@@ -66,3 +62,49 @@ typedef struct CTree
     CTBox node[100];
 
 }CTree;
+
+typedef enum{Link,Thread} Tag; 
+typedef struct BitNode
+{
+    int data;
+    struct BitNode *Lchild,*Rchild;
+    Tag LTag,RTag;
+}BitNode,*BiTree;
+
+BiTree pre;
+static InitTree(BiTree p)
+{
+    if(p)               //检测是否p为空，很重要，搭配递归
+        {
+        InitTree(p->Lchild);
+        if(!p->Lchild)
+            {
+                p->LTag=Thread;
+                p->Lchild=pre;
+            } 
+        if(!pre->Rchild)
+            {
+                pre->RTag=Thread;
+                pre->Rchild=p;
+            }
+        pre=p;
+        InitTree(p->Rchild);        
+        }
+    return OK;
+}
+
+static InOrder(BiTree Head)
+{   BiTree p=Head->Lchild;
+    while(p!=Head)             
+        {
+        while(!p->LTag)
+            {p=p->Lchild;}
+        printf("%c",p->data);       //无左才输出
+        while(p->RTag&&p->Rchild!=Head)     //是p->Rchild!=Head,而非p
+            {                                //p的右空则开始跳跃，直至遇到一个有右子树的节点，跳跃后的节点左子树肯定是被遍历过的
+                p=p->Rchild;                
+                printf("%c",p->data);
+            }  
+        p=p->Rchild;                //进入树的右分支，配合循环，看作对一个以p为根节点的子树的遍历；
+        }
+}
